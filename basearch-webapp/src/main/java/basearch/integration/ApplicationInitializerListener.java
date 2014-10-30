bf624ef60021516529a89bbe44fafecebfc38e75
Héctor <hectorlf@users.noreply.github.com>
2014-10-30 12:43:14 +0100

    Add shutdown code for logback's async appender

--- basearch-webapp/src/main/java/basearch/integration/ApplicationInitializerListener.java
+++ basearch-webapp/src/main/java/basearch/integration/ApplicationInitializerListener.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletContextListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ch.qos.logback.classic.LoggerContext;
 
 public class ApplicationInitializerListener implements ServletContextListener {
 
@@ -13,6 +14,8 @@ public class ApplicationInitializerListener implements ServletContextListener {
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
 		logger.debug("Shutting down application...");
+		// logback's async appender shutdown
+		((LoggerContext)LoggerFactory.getILoggerFactory()).stop();
 	}
 
 	@Override
