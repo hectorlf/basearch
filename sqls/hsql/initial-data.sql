ade145adaed74a239752c3ca81ee2a246347210e
Héctor <hectorlopezfernandez@hotmail.com>
2015-03-07 13:04:43 +0100

    Fix broken tests - a previous commit removed a user referenced in some tests

--- sqls/hsql/initial-data.sql
+++ sqls/hsql/initial-data.sql
@@ -7,6 +7,7 @@ insert into languages values (3,'en','US',null);
 insert into users values (1,'admin','$2a$10$fdCTnC8xIL5HhvFKJIGDiecG91buB6Lj9IB/US2xqxFiKhCcMCXBq', true, 1);
 insert into users values (2,'user','$2a$10$4qzMEbvFWnBPqxWLzFLGs.Mir//KKkvoorx6g47ogxxxpprvxMnse', true, 2);
 insert into users values (3,'disabled','$2a$10$W/D92o1yKHHkOvWsLo50jeNOPT2bCaZq3H3V.4gZ1B4.4ROyvSIjm', false, 3);
+insert into users values (4,'test','test', true, 1);
 
 -- Table: authorities
 insert into authorities values (1,'admin','ROLE_ADMIN');
