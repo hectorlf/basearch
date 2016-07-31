accc81d3569cc638b9e91dc96042877c16efd9fb
Héctor <hectorlopezfernandez@hotmail.com>
2016-07-31 15:00:39 +0200

    Upgrade to spring boot 1.4.0

--- src/test/java/basearch/test/BaseMvcTest.java
+++ src/test/java/basearch/test/BaseMvcTest.java
@@ -3,18 +3,17 @@ package basearch.test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import basearch.Application;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
-@WebIntegrationTest(randomPort=true)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class, webEnvironment=WebEnvironment.RANDOM_PORT)
 public abstract class BaseMvcTest {
 
 	@Autowired
--- src/test/java/basearch/test/BaseTest.java
+++ src/test/java/basearch/test/BaseTest.java
@@ -1,13 +1,13 @@
 package basearch.test;
 
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import basearch.Application;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class)
 public abstract class BaseTest {
 
 }
